b981e4be5b32405f9f03c434e7806e0cabc352e2
Taylor Sampson <taylor.sampson@ave81.com>
2019-12-02 16:47:21 -0600

    Day01

--- tests/Example.elm
+++ tests/Example.elm
@@ -42,7 +42,7 @@ suite : Test
 suite =
     describe "Advent of Code"
         [ describe "Day01"
-            [ test_answer "partOne" "ANSWERGOESHERE" Day01.partOne
-            , test_answer "partTwo" "ANSWERGOESHERE" Day01.partTwo
+            [ test_answer "partOne" "3477353" Day01.partOne
+            , test_answer "partTwo" "5213146" Day01.partTwo
             ]
         ]
